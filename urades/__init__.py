c8864a2f430062ea8eeb466804459479ce9824d3
Srivathsan2005 <rrsrivathsan@gmail.com>
2026-08-22 03:48:32 +0530

    Create __init__.py

---
+++ urades/__init__.py
@@ -0,0 +1,8 @@
+"""
+URADES — Unified Refractory Alloy Descriptor and Embrittlement Screener
+"""
+
+from .core import run_URADES, predict_case1, predict_case2, predict_case3, calc_GVI
+
+__version__ = "2.0.0"
+__author__  = "Srivathsan V, Dr. Chenna Krishna, Pravin Muneshwar"
